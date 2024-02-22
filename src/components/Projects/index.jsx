1dc9f214056cd9c3191aac8feacf929acfe0073f
JuanJoven01 <juanjov95@hotmail.com>
2024-02-22 12:46:55 -0500

    creating the project boxes

--- src/components/Projects/index.jsx
+++ src/components/Projects/index.jsx
@@ -1,26 +1,47 @@
-
+import { useNavigate} from "react-router-dom"
 
 const Projects = () => {
+
+    const navigate = useNavigate()
 
     const projects = [
         {
             'title': 'Blog',
-            'description': 'This is a personal Blog created in React with backend developed in FastAPI with SQLAlchemy',
-            'image': '*image Link'
+            'description': 'This is a personal blog created with React and Tailwind, a couple of times a week I add posts about news, components or tools that I find interesting.',
+            'image': '../../../public/img/reactTailwind.png'
         }
     ]
 
     return (
-        <section>
-            {
+        <section className=" bg-radial-gradient from-gradient_alpha via-gradient_bravo to-transparent to-70% backdrop-blur-sm ">
+            
+            <h1 className="text-center p-5 pt-20 text-5xl font-bold text-slate-300" >
+                Projects
+            </h1>
+
+            <div className="flex flex-wrap p-20 justify-center">
+                {
                 projects.map((item,index)=>(
-                    <project key={index}>
-                        <h3>{item['title']}</h3>
-                        <p>{item['description']}</p>
-                        <p>{item['image']}</p>
-                    </project>
-                ))
-            }
+                    <div key={index} className=' p-5 border-2 max-w-md bg-slate-800 flex-col'>
+                        <h3 className=" text-center font-satoshi-bolditalic text-2xl bg-gradient-to-r from-blue-500 to-delta text-transparent bg-clip-text">{item['title']}</h3>
+                        <p className=" text-center p-3 text-slate-300 font-satoshi-italic text-lg">{item['description']}</p>
+                        <img src={item['image']} alt="" className="h-20"/>
+                        <div className="flex ">
+                        <ul className="my-auto mr-5 flex">
+                            <li className='px-5 hover:cursor-pointer'  onClick={() => navigate('/')}>
+                                Portfolio
+                            </li>
+                            <li className='px-5 hover:cursor-pointer'  onClick={() => navigate('/')}>
+                                Blog
+                            </li>
+                        </ul>
+                        </div>
+                    </div>
+                    ))
+                }
+            </div>  
+            
+        
         </section>
     )
 }
